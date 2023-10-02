8e2069aaafa99050c3734fe5216144681ba985bf
5jisoo <56earls@gmail.com>
2023-10-02 15:46:15 +0900

    #6 Feat: mysql로 데이터 전처리 후 이동

---
+++ disease-data/preprocessing.py
@@ -0,0 +1,31 @@
+# import mysql.connector
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import datetime
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PW = os.getenv("MYSQL_PW")
+TABLE_NAME = "disease"
+
+engine = create_engine(f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PW}@127.0.0.1:3306/unibond_db?charset=utf8")
+connection = engine.connect()
+
+df = pd.read_csv("./disease-data/csv-data/rare_disease_2023_10_02_13_49_53.csv", encoding='utf-8')
+
+df = df[['질환명(한글)', '질환명(영문)']]
+
+df.rename(columns = {'질환명(한글)':'disease_name_kor','질환명(영문)':'disease_name_eng'},inplace=True)
+df['created_date'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+df['last_modified_date'] = df['created_date']
+df['status'] = "ACTIVE"
+
+df = df[['created_date', 'last_modified_date', 'disease_name_eng', 'disease_name_kor', 'status']]
+df.to_sql(name=TABLE_NAME,
+          con=connection,
+          if_exists='append', # or 'replace'
+          index=False) 
+
