10633273488c83fb48d0459f979925823bb7bf73
5jisoo <56earls@gmail.com>
2024-01-08 19:03:05 +0900

    #61 편지 조회 - 회원 차단 조건 추가

--- unibond/src/main/java/com/unibond/unibond/block/repository/MemberBlockRepository.java
+++ unibond/src/main/java/com/unibond/unibond/block/repository/MemberBlockRepository.java
@@ -2,9 +2,15 @@ package com.unibond.unibond.block.repository;
 
 import com.unibond.unibond.block.domain.MemberBlock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberBlockRepository extends JpaRepository<MemberBlock, Long> {
 
+    @Query("select count (mb) > 0 from MemberBlock mb " +
+            "where mb.reporter.id = :reporterId and mb.respondent.id = :respondentId ")
+    Boolean existsByReporterIdAndRespondentId(@Param("reporterId") Long reporterId,
+                                              @Param("respondentId") Long respondentId);
 }
--- unibond/src/main/java/com/unibond/unibond/comment/repository/CommentRepository.java
+++ unibond/src/main/java/com/unibond/unibond/comment/repository/CommentRepository.java
@@ -18,7 +18,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findCommentByIdAndStatus(Long id, BaseEntityStatus status);
 
     @Query("select distinct c from Comment c " +
-            "left join MemberBlock mb on ( c.member = mb.respondent and mb.reporter = :loginId )" +
+            "left join MemberBlock mb on ( c.member = mb.respondent and mb.reporter = :loginId ) " +
             "join fetch c.member " +
             "join fetch c.member.disease " +
             "where c.parentComment = :parentComment " +
